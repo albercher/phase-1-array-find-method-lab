1a2e1bd4288102897b3dd593be7dc0afb96274bc
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-02 13:03:59 -0400

    commit tree e4675d450a03e2c0a4c1ac855f1267413fd93991

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record){
+    const win = record.find(element => element.result === "W");
+    if (win === undefined){
+        return undefined
+    } else {
+        return win.year
+    }
+}
